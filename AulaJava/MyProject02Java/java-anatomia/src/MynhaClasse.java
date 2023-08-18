fc4a75cf59c092fe685fbf2384183b51b6674d48
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-18 16:59:37 -0300

    Update MynhaClasse.java

--- AulaJava/MyProject02Java/java-anatomia/src/MynhaClasse.java
+++ AulaJava/MyProject02Java/java-anatomia/src/MynhaClasse.java
@@ -1,6 +1,11 @@
 public class MynhaClasse {
   public static void main(String [] args) {
-    System.out.print("ola turma, senha bem-vindos");
+
+    // PALAVRAS MAIUSCULA POR COMPLETO, NÃO PODE SER ALTERADA; IMUTAVEL CONVENÇÃO UPPERCASE
+    final String BR = "Brasil";// DETERMINAR UMA INTEGRIDADE  CONSTANTE A UMA VARIAVEL QUE NÃO PODE SER ALTERADA
+   // CASO SEJÁ PALAVRA COMPOSTA IMUTAVEL USA-SE UPPERCASE SEPARANDO POR _ 
+    System.out.println(BR);
+
 
   } //corpo do metodo
 }
