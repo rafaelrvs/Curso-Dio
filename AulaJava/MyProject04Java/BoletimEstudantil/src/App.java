bc038c5d50847e883cae16dedf7541f58198ee5c
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-18 18:31:32 -0300

    update

---
+++ AulaJava/MyProject04Java/BoletimEstudantil/src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // codigo sem identação
+        /*
+         * int mediaFinal = 10;
+         * if(mediaFinal <6){
+         * System.out.println("Reprovado");
+         * }
+         * else if(mediaFinal == 6){
+         * System.out.println("Prova Minerva");
+         * }
+         * else{
+         * System.out.println("Aprovado");
+         * }
+         * 
+         */
+        // exemplo codigo identado
+        int mediaFinal = 10;
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("Prova Minerva");
+        else
+            System.out.println("Aprovado");
+
+    }
+}
