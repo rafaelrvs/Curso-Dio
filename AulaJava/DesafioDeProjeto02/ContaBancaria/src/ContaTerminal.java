22f8380ac29ac55b8f118e8328692ef0ad89174b
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-20 13:14:41 -0300

    update

--- AulaJava/DesafioDeProjeto02/ContaBancaria/src/ContaTerminal.java
+++ AulaJava/DesafioDeProjeto02/ContaBancaria/src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+      // TODO: Conhecer e importar a classe Scanner
+      // Exibir mensagem para nosso usuario
+      // Obter pelo scanner os valores digitados no terminal
+      // exibir a mensagem da conta criada
+  }
 }
