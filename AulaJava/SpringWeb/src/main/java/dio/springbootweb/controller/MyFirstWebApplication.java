c664218e28fc4f72eded0c218b1cb3ec7bb48cb1
rafaelrvs <rafaelvieira1333@gmail.com>
2023-09-13 13:30:12 -0300

    feat spring web

---
+++ AulaJava/SpringWeb/src/main/java/dio/springbootweb/controller/MyFirstWebApplication.java
@@ -0,0 +1,11 @@
+package dio.springbootweb.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstWebApplication {
+   public static void main(String[]args) {
+    SpringApplication.run(MyFirstWebApplication.class,args);
+   }
+}
